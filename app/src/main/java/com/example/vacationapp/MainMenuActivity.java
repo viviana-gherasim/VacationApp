5d44ad1d0df61a114201336b784b14ef079a7506
viviana-gherasim <viviana.gherasim@yahoo.com>
2022-02-05 20:13:04 +0200

    Implemented MainMenuActivity class.

--- app/src/main/java/com/example/vacationapp/MainMenuActivity.java
+++ app/src/main/java/com/example/vacationapp/MainMenuActivity.java
@@ -1,4 +1,55 @@
 package com.example.vacationapp;
 
-public class MainMenuActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button logout, profileButton, offerButton, spotButton;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+
+        logout=(Button) findViewById(R.id.logoutButton);
+        logout.setOnClickListener(this);
+
+        profileButton=(Button) findViewById(R.id.buttonProfileMenu);
+        profileButton.setOnClickListener(this);
+
+        offerButton=(Button) findViewById(R.id.buttonViewOffer);
+        offerButton.setOnClickListener(this);
+
+        spotButton = (Button) findViewById(R.id.buttonFindBestSpot);
+        spotButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logoutButton:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
+                break;
+
+//            case R.id.buttonProfileMenu:
+//                startActivity(new Intent(MainMenuActivity.this, ));
+//                break;
+//
+//            case R.id.buttonViewOffer:
+//                startActivity(new Intent(MainMenuActivity.this, ));
+//                break;
+//
+//            case R.id.buttonFindBestSpot:
+//                startActivity(new Intent(MainMenuActivity.this, ));
+//                break;
+        }
+    }
+
 }
+
